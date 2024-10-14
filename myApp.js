b2a5fe086ec909e01f4132dba161ec9f9f90bc51
Abril Jordan Casinillo <abriljordancasinillo@gmail.com>
2024-10-14 09:22:37 +0800

    helmet.js

--- myApp.js
+++ myApp.js
@@ -1,61 +1,13 @@
+const helmet = require('helmet');
 const express = require('express');
 const app = express();
 
+// Mount helmet.xssFilter middleware
+app.use(helmet.xssFilter());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Your routes
+app.get('/', function(req, res) {
+  res.send('Hello world');
+});
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
